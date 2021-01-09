68344d00c670f3d134172fec92d4ca9794b7809b
João Francisco Cocca Fukuda <joao.fukuda@protonmail.com>
2021-01-09 02:30:42 -0300

    refact(proxy): remove html bloat
    HTML was bloated because it was getting data in bytes array
    converting to str, then to String, then back to bytes
    
    Now I'll work only with bytes array. I've removed all unnecessary parts,
        but now I'll need to recreate the same behaviour only this time
        using the new type (no more bloat)

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,8 +1,6 @@
 use std::{ net, str, thread };
 use std::io::{ Read, Write };
 
-mod http;
-
 const BUFFER_SIZE: usize = 1024*1024;
 
 fn connect_streams(mut input: net::TcpStream, mut output: net::TcpStream)
@@ -31,7 +29,7 @@ fn connect_streams(mut input: net::TcpStream, mut output: net::TcpStream)
 	}
 }
 
-fn handle_http_request(mut client_stream: &net::TcpStream, request: http::Request)
+fn handle_http_request(mut client_stream: &net::TcpStream, raw_request: &[u8])
 {
 	println!("[INF] HTTP: Sending {} to {}:{}", request.method, request.host, request.port);
 
@@ -61,7 +59,7 @@ fn handle_http_request(mut client_stream: &net::TcpStream, request: http::Reques
 	client_stream.write(response.as_bytes()).unwrap();
 }
 
-fn handle_tcp_connection(mut client_stream: net::TcpStream, request: http::Request)
+fn handle_tcp_connection(mut client_stream: net::TcpStream, raw_request: &[u8])
 {
 	if let Ok(server_stream) = net::TcpStream::connect(format!("{}:{}", request.host, request.port))
 	{
@@ -83,29 +81,25 @@ fn handle_tcp_connection(mut client_stream: net::TcpStream, request: http::Reque
 	}
 	else
 	{
-		client_stream.write(String::from("HTTP/1.1 500 ERROR\r\n\r\n").as_bytes()).unwrap();
+		client_stream.write("HTTP/1.1 500 ERROR\r\n\r\n".as_bytes()).unwrap();
 	}
 }
 
 pub fn handle_client(mut client_stream: net::TcpStream)
 {
-	let request: http::Request;
 	let buf = &mut [0u8; BUFFER_SIZE];
 
 	if let Ok(data_size) = client_stream.read(buf)
 	{
 		if data_size != 0
 		{
-			let request_string = str::from_utf8(&buf[0 .. data_size]).unwrap();
-			request = http::Request::from_proxy(request_string.to_string()).unwrap();
-
-			if request.is_connection()
+			if str::from_utf8(&buf[0 .. 7]).unwrap() == "CONNECT"
 			{
-				handle_tcp_connection(client_stream, request);
+				handle_tcp_connection(client_stream, buf);
 			}
 			else
 			{
-				handle_http_request(&client_stream, request);
+				handle_http_request(&client_stream, buf);
 			}
 		}
 	}
