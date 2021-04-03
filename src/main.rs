054323b558d3f514833907214d610a345e75a67d
João Fukuda <joao.fukuda@protonmail.com>
2021-04-03 13:08:01 -0300

    Update (I don't know when or what) of the proxy
    The proxy architecture will change a bit to support both http and socks4
    proxy protocols (as I liked working with socks4 better than working
          with http proxies).
    
    Will change to C++ soon, so I'll keep y'all updated on another branch:
    `to_cpp`.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::{ net, thread };
 
+use proxy::http::HttpProxy;
+
 mod proxy;
+mod http;
+mod tls;
 
 fn main()
 {
-	let host = net::TcpListener::bind("0.0.0.0:3128").unwrap();
-	println!("Starting proxy server on 0.0.0.0:3128!");
+	let proxy: HttpProxy = HttpProxy::new("0.0.0.0:3128");
 
-	for client in host.incoming()
-	{
-		thread::Builder::new().name("client handling".to_string()).spawn(move|| proxy::handle_client(client.unwrap())).unwrap();
+	for client in proxy.incomming() {
+		thread::spawn(move|| proxy::handle_client(client.unwrap()));
 	}
 }
+
